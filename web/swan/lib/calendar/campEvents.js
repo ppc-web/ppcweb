d9b5276cb7b021aab8160b68338bc8f568588673
Xingang Huang <xingang@yahoo-inc.com>
2016-11-13 16:14:51 -0800

    add league and camp event.

---
+++ web/swan/lib/calendar/campEvents.js
@@ -0,0 +1,46 @@
+events.push.apply(events,
+[
+ 	{
+		title: '- 5p <center> Camp </center>',
+		start: '2016-11-21T09:15:00',
+		end: '2016-11-21T12:15:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	},
+	{
+		title: '- 5p <center> Camp </center>',
+		start: '2016-11-21T14:00:00',
+		end: '2016-11-21T17:00:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	},
+	
+	{
+		title: '- 1215p <center> Camp </center>',
+		start: '2016-11-22T09:15:00',
+		end: '2016-11-22T12:15:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	},
+	{
+		title: '- 5p <center> Camp </center>',
+		start: '2016-11-22T14:00:00',
+		end: '2016-11-22T17:00:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	},
+	{
+		title: '- 1215p <center> Camp </center>',
+		start: '2016-11-23T09:15:00',
+		end: '2016-11-23T12:15:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	},
+	{
+		title: '- 5p <center> Camp </center>',
+		start: '2016-11-23T14:00:00',
+		end: '2016-11-23T17:00:00',
+		url: "http://www.swanpp.com/group.html",
+		color: "OliveDrab"
+	}
+	]);
---
+++ web/swan/lib/calendar/leagueEvents.js
@@ -0,0 +1,52 @@
+events.push.apply(events,
+[
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-10-30T14:00:00',
+		end: '2016-10-30T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-11-06T14:00:00',
+		end: '2016-11-06T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-11-13T14:00:00',
+		end: '2016-11-13T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-11-27T14:00:00',
+		end: '2016-11-27T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-12-04T14:00:00',
+		end: '2016-12-04T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-12-11T14:00:00',
+		end: '2016-12-11T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	},
+	{
+		title: '- 5p <center> Sunday League </center>',
+		start: '2016-12-18T14:00:00',
+		end: '2016-12-18T17:00:00',
+		url: 'http://ayiwei.com/vcac',
+		color: "RoyalBlue"
+	}
+]);
