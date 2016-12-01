d37709ad2f65b5554093b71286011c0aae40ba01
Xingang Huang <xingang@yahoo-inc.com>
2016-12-01 10:40:36 -0800

    modify free kid group class time.

--- web/swan/lib/calendar/freeKidGroup.js
+++ web/swan/lib/calendar/freeKidGroup.js
@@ -2,25 +2,27 @@ eventSources.push(
 {
 	events: [
 		{
-		    title:"- 4p <center> Free Kid Group Class </center>",
-		    start: '15:00',
-		    end: '16:00', 
+		    title:"- 430p <center> Free Kid Group Class </center>",
+		    start: '15:30',
+		    end: '16:30', 
 		    dow: [ 1,2,3,4 ], // Repeat monday to thursday
 		    url: 'https://goo.gl/nFhNhm',
 		    ranges:[{start:"2016-11-27", end:"2016-12-02"}]
 		},
 		{
-		    title:"- 4p <center> Free Kid Group Class </center>",
-		    start: '15:00',
-		    end: '16:00', 
+		    title:"- 430p <center> Free Kid Group Class </center>",
+		    start: '15:30',
+		    end: '16:30', 
 		    dow: [ 1,2,3,4 ], // Repeat monday to thursday
+		    url: 'https://goo.gl/73nsP8',
 		    ranges:[{start:"2016-12-04", end:"2016-12-10"}]
 		},
 		{
-		    title:"- 4p <center> Free Kid Group Class </center>",
-		    start: '15:00',
-		    end: '16:00', 
+		    title:"- 430p <center> Free Kid Group Class </center>",
+		    start: '15:30',
+		    end: '16:30', 
 		    dow: [ 1,2,3,4 ], // Repeat monday to thursday
+		    url: 'https://goo.gl/tVdyCC',
 		    ranges:[{start:"2016-12-11", end:"2016-12-17"}]
 		},
 	],
