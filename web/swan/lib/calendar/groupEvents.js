064e43b23b82257af558880e0881a09643a0aa43
Xingang Huang <xingang@yahoo-inc.com>
2016-12-05 05:25:08 -0800

    more change for free.

--- web/swan/lib/calendar/groupEvents.js
+++ web/swan/lib/calendar/groupEvents.js
@@ -4,56 +4,56 @@ events.push.apply(events,
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-11-14T18:00:00',
 		end: '2016-11-14T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-11-16T18:00:00',
 		end: '2016-11-16T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-11-28T18:00:00',
 		end: '2016-11-28T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-11-30T18:00:00',
 		end: '2016-11-30T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-12-05T18:00:00',
 		end: '2016-12-05T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-12-07T18:00:00',
 		end: '2016-12-07T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-12-12T18:00:00',
 		end: '2016-12-12T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	},
 	{
 		title: '- 8p <center> Kid Group Class </center>',
 		start: '2016-12-14T18:00:00',
 		end: '2016-12-14T20:00:00',
-		url: "http://www.swanpp.com/group.html",
+		url: "http://www.swanpp.com/group.html#paid",
 		color: "Orchid"
 	}
 	
