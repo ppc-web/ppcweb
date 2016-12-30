07e59d555decf3ff305ebda012ac00bd9f912559
Xingang Huang <xingang@yahoo-inc.com>
2016-12-30 11:26:23 -0800

    add u3700 to main page.

--- web/swan/lib/calendar/u3700.js
+++ web/swan/lib/calendar/u3700.js
@@ -5,54 +5,63 @@ eventSources.push(
 		    title:"<center> U3700 <br/> Christmas Warmup <br/> 8 tables reserved </center>",
 		    start: '2016-12-25T14:30',
 		    end: '2016-12-25T19:30',
+		    url: "http://swanpp.com/u3700/warmup.html",
 		    color: "Magenta"
 		},
  		{
 		    title:"<center> U3700 <br/> New Year Warmup <br/> 8 tables reserved </center>",
 		    start: '2017-01-01T14:30',
 		    end: '2017-01-01T19:30',
+		    url: "http://swanpp.com/u3700/warmup.html",
 		    color: "Magenta"
 		},
 		{
 		    title:"<center> U3700 <br/> Group A, E <br/> 8 tables reserved </center>",
 		    start: '2017-01-07T17:30',
 		    end: '2017-01-07T21:30',
+		    url: "http://swanpp.com/u3700/draw.html",
 		    color: "Blue"
 		},
 		{
 		    title:"<center> U3700 <br/> Group B, F <br/> 8 tables reserved </center>",
 		    start: '2017-01-14T17:30',
 		    end: '2017-01-14T21:30',
+		    url: "http://swanpp.com/u3700/draw.html",
 		    color: "Blue"
 		},
 		{
 		    title:"<center> U3700 <br/> Group C, G <br/> 8 tables reserved </center>",
 		    start: '2017-01-21T17:30',
 		    end: '2017-01-21T21:30',
+		    url: "http://swanpp.com/u3700/draw.html",
 		    color: "Blue"
 		},
 		{
 		    title:"<center> U3700 <br/> Group D <br/> 8 tables reserved </center>",
 		    start: '2017-01-28T17:30',
 		    end: '2017-01-28T21:30',
+		    url: "http://swanpp.com/u3700/draw.html",
 		    color: "Blue"
 		},
 		{
 		    title:"<center> U3700 Lost Pearl 1<br/> Double E<br/> 5 tables reserved </center>",
 		    start: '2017-02-04T17:30',
 		    end: '2017-02-04T21:30',
+		    url: "http://swanpp.com/u3700/schedule.html",
 		    color: "#ff9933"
 		},
 		{
 		    title:"<center> U3700 Lost Pearl 2<br/> Double E<br/> 5 tables reserved </center>",
 		    start: '2017-02-05T13:30',
 		    end: '2017-02-05T17:30',
+		    url: "http://swanpp.com/u3700/schedule.html",
 		    color: "#ff9933"
 		},
 		{
 		    title:"<center> U3700 <br/> Final SE<br/> 8 tables reserved </center>",
 		    start: '2017-02-11T14:30',
 		    end: '2017-02-11T19:30',
+		    url: "http://swanpp.com/u3700/schedule.html",
 		    color: "#1D7018"
 		},
  	]
