ce740e2628d6846d6f9cbe8a456ff71b55ae233b
Xingang Huang <xingang@yahoo-inc.com>
2017-04-30 18:53:17 -0700

    add anniversary party.

--- web/swan/lib/calendar/partyEvents.js
+++ web/swan/lib/calendar/partyEvents.js
@@ -12,5 +12,12 @@ events.push.apply(events,
 		end: '2016-11-23T22:00:00',
 		color: "SlateBlue"
 	},
+	{
+		title: '<center> PPC Anniversary Party <br/>No Open Play after 5pm. </center>',
+		start: '2017-05-13T17:00:00',
+		end: '2017-05-13T23:30:00',
+		url: "ppc-ann-chinese.html",
+		color: "Magenta"
+	},
 	
 ]);
