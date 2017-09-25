8a0be63f3a06ca82b6e8e6242649ea482e2fd82d
Xingang Huang <xingang@yahoo-inc.com>
2017-09-25 13:50:04 -0700

    update.

--- web/swan/lib/calendar/freeKidGroup.js
+++ web/swan/lib/calendar/freeKidGroup.js
@@ -239,6 +239,14 @@ eventSources.push(
 		    url: 'https://docs.google.com/spreadsheets/d/1-02w2bnpMWOetZdED404w0XdkmELXlQjzQiMuOaU9NI/edit#gid=2072644612',
 		    ranges:[{start:"2017-08-30", end:"2017-09-27"}]
 		},
+ 		{
+		    title:"<center> Free Kid Group</center>",
+		    start: '19:00',
+		    end: '20:30', 
+		    dow: [ 3 ], // Wed
+		    url: 'https://docs.google.com/spreadsheets/d/1-02w2bnpMWOetZdED404w0XdkmELXlQjzQiMuOaU9NI/edit#gid=1495373460',
+		    ranges:[{start:"2017-10-01", end:"2017-10-31"}]
+		},
 		
 		{
 		    title:"<center> Free Kid Group</center>",
@@ -288,6 +296,14 @@ eventSources.push(
 		    url: 'https://docs.google.com/spreadsheets/d/1C-f_FHm1EFivryZYxzu0xNb7wMHlK_JDShUiO6Hz5pY/edit#gid=478110221',
 		    ranges:[{start:"2017-09-09", end:"2017-09-30"}]
 		},
+		{
+		    title:"<center> Free Kid Group</center>",
+		    start: '15:30',
+		    end: '17:00', 
+		    dow: [ 6 ], // Sat
+		    url: 'https://docs.google.com/spreadsheets/d/1C-f_FHm1EFivryZYxzu0xNb7wMHlK_JDShUiO6Hz5pY/edit#gid=1485134427',
+		    ranges:[{start:"2017-10-01", end:"2017-10-31"}]
+		},
 	], 
 	color: "Orchid"
 });
@@ -334,6 +350,14 @@ eventSources.push(
 				    dow: [ 1 ], // Mon
 				    url: 'https://docs.google.com/spreadsheets/d/1e7yA8Awr71d5T0IyG-aWMWfYMBYiUPHXRnAtXT8RHeI/edit#gid=1252093407',
 				    ranges:[{start:"2017-09-11", end:"2017-09-25"}]
+				},
+		 		{
+				    title:"<center> Adult Group</center>",
+				    start: '19:00',
+				    end: '20:30', 
+				    dow: [ 1 ], // Mon
+				    url: 'https://docs.google.com/spreadsheets/d/1e7yA8Awr71d5T0IyG-aWMWfYMBYiUPHXRnAtXT8RHeI/edit#gid=848305995',
+				    ranges:[{start:"2017-10-01", end:"2017-10-31"}]
 				},
 				
 
