ac8f48a56d0b2f83d7e8de78dc7ad9e26bb01b64
Audrey <blueshadow1125@gmail.com>
2020-07-19 23:31:13 -0700

    minor changes

--- web/donation/users/init.php
+++ web/donation/users/init.php
@@ -27,10 +27,10 @@ require_once $abs_us_root.$us_url_root.'users/helpers/helpers.php';
 // Set config
 $GLOBALS['config'] = array(
 	'mysql'      => array('host'         => 'localhost',
-                          'username'     => 'root',
-                          'port' => '3306',
-                          'password'     => '',
-                          'db'           => 'ppc',
+'username'     => 'root',
+'port' => '3306',
+'password'     => '',
+'db'           => 'ppc',
 ),
 'remember'        => array(
   'cookie_name'   => 'pmqesoxiw318374csb',
