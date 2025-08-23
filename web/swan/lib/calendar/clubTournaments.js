97ddadfd784f27fd444572198f210511b3d6257b
Welly-git <ppcswan.admn@gmail.com>
2025-08-23 02:28:45 -0700

    auto commit msg

---
+++ web/swan/lib/calendar/clubTournaments.js
@@ -0,0 +1,41 @@
+events.push.apply(events,
+[
+	{
+		title: '<center> Swan Doubles u3500 </center>',
+		start: '2025-07-26T09:00:00',
+		end: '2025-07-26T17:00:00',
+		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},
+		{
+		title: '<center> Swan Doubles u3200 </center>',
+		start: '2025-08-09T09:00:00',
+		end: '2025-08-09T17:00:00',
+		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},
+		{
+		title: '<center> Swan Doubles u3800 </center>',
+		start: '2025-08-02T09:00:00',
+		end: '2025-08-02T17:00:00',
+		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},
+		{
+		title: '<center> Swan Doubles u4200 </center>',
+		start: '2025-08-16T09:00:00',
+		end: '2025-08-16T17:00:00',
+		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},
+	{
+		title: '<center> 🏓CLC </center>',
+		start: '2025-04-16T12:00:00',
+		end: '2025-05-16T21:00:00',
+		url: "https://www.swanpp.com/2025_May_clc/index.html",
+		color: "CadetBlue"
+	},	
+	
+	
+	
+]);
