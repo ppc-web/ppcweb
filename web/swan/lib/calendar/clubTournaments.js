56f84cc4a3c662aae06a600f8786ef7bc6ae2c89
Welly-git <ppcswan.admn@gmail.com>
2025-08-23 15:42:20 -0700

    auto commit msg

--- web/swan/lib/calendar/clubTournaments.js
+++ web/swan/lib/calendar/clubTournaments.js
@@ -1,28 +1,28 @@
 events.push.apply(events,
 [
 	{
-		title: '<center> Swan Doubles u3500 </center>',
+		title: '<center> 🏓Swan Doubles u3500 </center>',
 		start: '2025-07-26T09:00:00',
 		end: '2025-07-26T17:00:00',
 		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
 		color: "CadetBlue"
 	},
 		{
-		title: '<center> Swan Doubles u3200 </center>',
+		title: '<center> 🏓Swan Doubles u3200 </center>',
 		start: '2025-08-09T09:00:00',
 		end: '2025-08-09T17:00:00',
 		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
 		color: "CadetBlue"
 	},
 		{
-		title: '<center> Swan Doubles u3800 </center>',
+		title: '<center> 🏓Swan Doubles u3800 </center>',
 		start: '2025-08-02T09:00:00',
 		end: '2025-08-02T17:00:00',
 		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
 		color: "CadetBlue"
 	},
 		{
-		title: '<center> Swan Doubles u4200 </center>',
+		title: '<center> 🏓Swan Doubles u4200 </center>',
 		start: '2025-08-16T09:00:00',
 		end: '2025-08-16T17:00:00',
 		url: "https://www.swanpp.com/2025_SwanDoubles/index.html",
@@ -35,7 +35,32 @@ events.push.apply(events,
 		url: "https://www.swanpp.com/2025_May_clc/index.html",
 		color: "CadetBlue"
 	},	
-	
-	
-	
+	{
+		title: '<center> 🏓Doubles u3200 u3800</center>',
+		start: '2024-10-12T09:00:00',
+		end: '2024-10-12T18:00:00',
+		url: "https://www.swanpp.com/2024_Fall_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},
+	{
+		title: '<center> 🏓Doubles u3500 u4200</center>',
+		start: '2024-10-19T09:00:00',
+		end: '2024-10-19T18:00:00',
+		url: "https://www.swanpp.com/2024_Fall_SwanDoubles/index.html",
+		color: "CadetBlue"
+	},	
+	{
+		title: '<center> 🏓CLC Div1&Div2 </center>',
+		start: '2024-09-21T13:00:00',
+		end: '2024-09-21T19:00:00',
+		url: "https://www.swanpp.com/2024_Sept_clc/index.html",
+		color: "CadetBlue"
+	},	
+		{
+		title: '<center> 🏓CLC March </center>',
+		start: '2024-03-09T13:00:00',
+		end: '2024-03-09T19:00:00',
+		url: "https://www.swanpp.com/2024_Mar_clc/index.html",
+		color: "CadetBlue"
+	},	
 ]);
